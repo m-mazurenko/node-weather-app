f56a9b9f19a6fd9a48cdc87cca451e30cce94b89
nickolamaz <nickolay.ma@gmail.com>
2021-10-18 13:40:21 +0300

    Fix API search

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,16 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=db727bb8a5c2637944fe8c9cbd58df3a&query=' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
+        console.log(body)
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoibmlja3ktbWF6IiwiYSI6ImNrdXdpaGc1NjJxNjEyd3FyN2gxMXJ2bjQifQ.bI4WEEdyXT7U-08nI6wwkQ'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
